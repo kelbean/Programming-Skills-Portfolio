9a27464ff081480718edb2690e659c950cc04554
Kelvin <projectwave00@gmail.com>
2024-10-26 01:19:04 +0400

    Added exercise 9

---
+++ Exercise_9.py
@@ -0,0 +1,28 @@
+"""
+
+Exercise 09 - Hello
+By: Kelvin Bautista
+CCL4
+
+"""
+
+# The function below contains a command that prints hello:
+
+def hello():
+    print("Hello")
+
+# The function below is another one that represents the main one, meaning this handles all the main commands.
+
+def main():
+    hello() # This calls the hello() function which will print "Hello"
+
+if __name__ == "__main__":
+    main()
+
+'''
+
+Comments:
+Although this was simply just a fill in the blank exercise, it helped me
+understand functions better.
+
+'''
