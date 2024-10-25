cfbe02751af80d4b555f5ddf7ba81566101ff4d8
Kelvin <projectwave00@gmail.com>
2024-10-26 00:36:31 +0400

    Added exercise 1

---
+++ Exercise_1.py
@@ -0,0 +1,27 @@
+"""
+
+Exercise 01 - Coding is Cool
+By: Kelvin Bautista
+CCL4
+
+"""
+
+# Variables
+
+word1 = "Coding "    # This variable holds the first word in the phrase.
+word2 = "is "        # This variable holds the second word in the phrase.
+word3 = "Cool"       # This variable holds the LAST word in the phrase.
+
+# Printing out the statement
+
+print(word1 + word2 + word3) # With all the variables combined, it displays the statement: "Coding is Cool"
+
+
+
+'''
+
+Comments:
+It's pretty simple for a first exercise. With the help of string concatenation, we can
+combine all the variables together to complete the phrase.
+
+'''
